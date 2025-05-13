8ef2d438fc0f62b397bb4625df53c96636e8e5fe
Ding0805 <2842279244@qq.com>
2025-05-13 14:23:04 +0800

    Update test_model.py

--- model/test_model.py
+++ model/test_model.py
@@ -106,12 +106,12 @@ class QAModel(pl.LightningModule):
                 activations=(None, None), vector_gate=True)
         )
 
-        # GVPConvLayer 参数
+        # GVPConvLayer 
         self.gvp = nn.ModuleList(GVPConvLayer(
-            node_dims=self.node_hidden_dim,  # 节点特征维度
-            edge_dims=(self.edge_scalar_feature_dim, self.edge_vector_feature_dim),  # 边特征维度
-            activations=(F.relu, None),  # 激活函数（标量，矢量）
-            vector_gate=True  # 是否使用矢量门控
+            node_dims=self.node_hidden_dim, 
+            edge_dims=(self.edge_scalar_feature_dim, self.edge_vector_feature_dim),  
+            activations=(F.relu, None),  
+            vector_gate=True 
         ) for _ in range(_gvp_num_layer))
 
         ns, _ = (
@@ -128,49 +128,30 @@ class QAModel(pl.LightningModule):
             nn.Dropout(p=0.1),  # Add a Dropout layer
         )
 
-        self.mlp_readout = MLPReadoutlddtClassV3(input_dim=self.node_out_dim, output_dim=1, dp_rate=_mlp_dp_rate)
+        self.mlp_readout = MLPReadoutlddtClass(input_dim=self.node_out_dim, output_dim=1, dp_rate=_mlp_dp_rate)
 
     def nt_xent_loss(self, anchor, positive, negatives, anchor_score, neg_scores, anchor_target, neg_targets,
-                     temperature=0.5):
-        """
-        计算基于标签权重的 NT-Xent 对比损失。
+                     temperature=0.07):
 
-        :param anchor: 主样本嵌入
-        :param positive: 正样本嵌入
-        :param negatives: 负样本嵌入 (list)
-        :param neg_scores: 负样本对应的 DockQ 分数 (list)
-        :param anchor_score: 主样本的 DockQ 分数
-        :param neg_targets: 负样本对应的 target 名称 (list)
-        :param anchor_target: 主样本的 target 名称
-        :param temperature: 温度参数
-        :return: 计算出的对比损失
-        """
-
-        # 计算主样本与正样本的相似度
         pos_similarity = torch.exp(torch.cosine_similarity(anchor, positive) / temperature)
 
         neg_similarities = []
         neg_weights = []
 
         for negative, neg_score, neg_target in zip(negatives, neg_scores, neg_targets):
-            # 计算负样本相似度
             neg_similarity = torch.exp(torch.cosine_similarity(anchor, negative) / temperature)
             neg_similarities.append(neg_similarity)
 
-            # 计算权重：同一 target 下用分数差值，不同 target 设为 1
             weight = torch.abs(anchor_score - neg_score) if neg_target == anchor_target else torch.ones_like(
                 anchor_score)
             weight = weight.squeeze()
             neg_weights.append(weight)
 
-        # 堆叠所有负样本相似度 & 权重
         neg_similarities = torch.stack(neg_similarities)  # (num_negatives,)
         neg_weights = torch.stack(neg_weights)  # (num_negatives,)
 
-        # 计算加权负样本相似度之和
         weighted_neg_sum = (neg_similarities * neg_weights).sum(dim=0)
 
-        # 计算损失
         loss = -torch.log(pos_similarity / (pos_similarity + weighted_neg_sum)).mean()
 
         return loss
@@ -178,7 +159,6 @@ class QAModel(pl.LightningModule):
     def forward(self, anchor_graph, positive_graph=None, negative_graphs=None, anchor_score=None, neg_scores=None,
                 anchor_target=None, neg_targets=None, scatter_mean=True, dense=True):
 
-        # 主样本嵌入
         anchor_h = anchor_graph.ndata['feat'].to(self.device).float()
         anchor_x = anchor_graph.ndata['coords'].to(self.device).float()
         anchor_o = anchor_graph.ndata['ori'].to(self.device).float()
@@ -194,10 +174,8 @@ class QAModel(pl.LightningModule):
         anchor_edge_inputs = (anchor_e, anchor_edge_vectors_normalized)
         anchor_edge_inputs = self.W_e(anchor_edge_inputs)
         anchor_node_inputs = self.W_v(anchor_h)
-
-        # 添加残差连接
         tensor1, tensor2 = anchor_node_inputs
-        tensor2 = tensor2 + anchor_o  # 将 ori 加到 tensor2 上
+        tensor2 = tensor2 + anchor_o 
 
         anchor_node_inputs = (tensor1, tensor2)
 
@@ -207,18 +185,14 @@ class QAModel(pl.LightningModule):
         anchor = self.W_out(anchor_node_inputs)
         anchor_z = self.dense(anchor)
 
-        # 主样本图级嵌入
         anchor_graph.ndata['feat'] = anchor_z
         anchor_final_zg = dgl.mean_nodes(anchor_graph, 'feat')
 
-        # 通过MLP层（多层感知机）进行最终预测
-        x, y_level = self.mlp_readout(anchor_final_zg)  # 期望 pred_lddt 形状为 [batch_size, 1]
+        x, y_level = self.mlp_readout(anchor_final_zg)  # [batch_size, 1]
         pred_lddt = x
         pred_lddt_level = y_level
 
-        # 如果正负样本图存在，则计算对比损失（如果对比学习还是需要）
         if positive_graph is not None and negative_graphs is not None:
-            # 正样本嵌入
             positive_h = positive_graph.ndata['feat'].to(self.device).float()
             positive_x = positive_graph.ndata['coords'].to(self.device).float()
             positive_o = positive_graph.ndata['ori'].to(self.device).float()
@@ -235,16 +209,14 @@ class QAModel(pl.LightningModule):
             positive_edge_inputs = (positive_e, positive_edge_vectors_normalized)
             positive_edge_inputs = self.W_e(positive_edge_inputs)
             positive_node_inputs = self.W_v(positive_h)
-            # 添加残差连接
             tensor1, tensor2 = positive_node_inputs
-            tensor2 = tensor2 + positive_o  # 将 ori 加到 tensor2 上
+            tensor2 = tensor2 + positive_o
             positive_node_inputs = (tensor1, tensor2)
             for layer in self.gvp:
                 positive_node_inputs = layer(positive_node_inputs, positive_edge_index, positive_edge_inputs)
             positive = self.W_out(positive_node_inputs)
             positive_z = self.dense(positive)
 
-            # 负样本嵌入
             negative_zs = []
             for negative_graph in negative_graphs:
                 negative_h = negative_graph.ndata['feat'].to(self.device).float()
@@ -263,9 +235,8 @@ class QAModel(pl.LightningModule):
                 negative_edge_inputs = (negative_e, negative_edge_vectors_normalized)
                 negative_edge_inputs = self.W_e(negative_edge_inputs)
                 negative_node_inputs = self.W_v(negative_h)
-                # 添加残差连接
                 tensor1, tensor2 = negative_node_inputs
-                tensor2 = tensor2 + negative_o  # 将 x 加到 tensor2 上
+                tensor2 = tensor2 + negative_o 
                 negative_node_inputs = (tensor1, tensor2)
                 for layer in self.gvp:
                     negative_node_inputs = layer(negative_node_inputs, negative_edge_index, negative_edge_inputs)
@@ -273,24 +244,20 @@ class QAModel(pl.LightningModule):
                 negative_z = self.dense(negative)
                 negative_zs.append(negative_z)
 
-            # 主样本图级嵌入
             anchor_graph.ndata['feat'] = anchor_z
             anchor_zg = dgl.mean_nodes(anchor_graph, 'feat')
 
-            # 正样本图级嵌入
             positive_graph.ndata['feat'] = positive_z
             positive_zg = dgl.mean_nodes(positive_graph, 'feat')
 
-            # 负样本图级嵌入
             negative_zgs = []
             for negative_graph, negative_z in zip(negative_graphs, negative_zs):
                 negative_graph.ndata['feat'] = negative_z
                 negative_zg = dgl.mean_nodes(negative_graph, 'feat')
                 negative_zgs.append(negative_zg)
 
-            # 计算对比损失
             contrast_loss = self.nt_xent_loss(anchor_zg, positive_zg, negative_zgs,
                                               anchor_score, neg_scores, anchor_target, neg_targets)
             return pred_lddt, pred_lddt_level, contrast_loss
 
-        return pred_lddt, pred_lddt_level  # 如果没有正负样本图，只返回预测
+        return pred_lddt, pred_lddt_level 
